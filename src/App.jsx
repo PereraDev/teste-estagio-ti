f85e6d9a27d057386bca6758596f35839e324d44
Alex Pereira <pereraacompany@gmail.com>
2026-07-05 13:43:26 -0300

    feat: adiciona alternancia de tema claro/escuro

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import Header from "./components/Header.jsx";
-import Sobre from "./components/Sobre.jsx";
-import Formacao from "./components/Formacao.jsx";
-import Conhecimentos from "./components/Conhecimentos.jsx";
-import Contato from "./components/Contato.jsx";
+import { useState, useEffect } from 'react';
+import Header from './components/Header.jsx';
+import Sobre from './components/Sobre.jsx';
+import Formacao from './components/Formacao.jsx';
+import Conhecimentos from './components/Conhecimentos.jsx';
+import Contato from './components/Contato.jsx';
 
 function App() {
+  const [tema, setTema] = useState("claro");
+  useEffect(() => {document.documentElement.setAttribute("data-theme", tema)}, [tema]);
   return (
     <>
+    <div style={{display: 'flex', justifyContent: 'flex-end', padding: '10px'}}>
+      <button onClick={() => setTema(tema === "claro" ? "escuro" : "claro")}>
+        Trocar Tema
+      </button>
+    </div>
       <Header />
       <Sobre />
       <Formacao />
